2257c4639e3c3a039c4c506152cc9a8c2b7066c6
Fanan Dala <fanan.dala@yahoo.com>
2020-10-30 18:20:11 +0100

    config: setup bootstrap-vue

--- src/main.js
+++ src/main.js
@@ -2,7 +2,11 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import { BootstrapVue } from 'bootstrap-vue'
+import 'bootstrap-vue/dist/bootstrap-vue.css'
+import "./assets/sass/main.scss";
 
+Vue.use(BootstrapVue)
 Vue.config.productionTip = false
 
 new Vue({
