3130d642c92a36afa709aab929aadc9d39afb9d0
Fanan Dala <fanan.dala@yahoo.com>
2020-10-30 20:36:42 +0100

    build: added v-movable

--- src/main.js
+++ src/main.js
@@ -2,11 +2,13 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import movable from "v-movable";
 import { BootstrapVue } from 'bootstrap-vue'
 import 'bootstrap-vue/dist/bootstrap-vue.css'
 import "./assets/sass/main.scss";
 
 Vue.use(BootstrapVue)
+Vue.use(movable)
 Vue.config.productionTip = false
 
 new Vue({
