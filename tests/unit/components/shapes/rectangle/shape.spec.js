ef5f3bc3cbfc0601b9f25e1bc405f8db46fe6943
Fanan Dala <fanan.dala@yahoo.com>
2020-11-02 11:15:41 +0100

    test: add rectangle shape test

---
+++ tests/unit/components/shapes/rectangle/shape.spec.js
@@ -0,0 +1,31 @@
+import { shallowMount } from '@vue/test-utils';
+import RectangleShape from '@/components/shapes/rectangle/Shape.vue';
+
+describe('CircleShape displays a circle based on props', () => {
+    it('displays an SVG circle with appropriate attributes', () => {
+        const shape = {
+            top: 100,
+            left: 100,
+            color: "#276db8",
+            component: 'rectangle-shape',
+            key: 'rectangle',
+            parameters: {
+                width: 40,
+                height: 20,
+            }
+        }
+
+        const wrapper = shallowMount(RectangleShape, {
+            propsData: {shape}
+        })
+
+        const svg = wrapper.find('svg')
+        expect(svg.attributes('height')).toBe("" + shape.parameters.height)
+        expect(svg.attributes('width')).toBe("" + shape.parameters.width)
+
+        const rect = wrapper.find('rect')
+        expect(rect.attributes('width')).toBe("" + shape.parameters.width)
+        expect(rect.attributes('height')).toBe("" + shape.parameters.height)
+        expect(rect.attributes('fill')).toBe(shape.color)
+    })
+})
